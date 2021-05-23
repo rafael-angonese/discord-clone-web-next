283983d238ea291cbeb7bcc8a6b7e30de5b05664
Rafael Angonese <rafael.angonese09@gmail.com>
2021-05-23 20:11:57 -0300

    motion load on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+
+import {
+    forwardRef,
+    ChakraProps,
+    chakra,
+    Container,
+    ComponentWithAs
+} from '@chakra-ui/react';
+import { motion, MotionProps } from 'framer-motion';
+
+import withAuth from '../utils/withAuth';
+
+export type MotionBoxProps = Omit<ChakraProps, keyof MotionProps> &
+    MotionProps & {
+        as?: React.ElementType;
+    };
+
+const MotionBox = motion(
+    forwardRef<ChakraProps, 'div'>((props, ref) => {
+        return <chakra.div ref={ref} {...props} />;
+    })
+) as ComponentWithAs<'div', MotionBoxProps>;
 
 const Home: React.FC = () => {
-  return (
-    <>
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <Heading size='sm'>I'm a Heading</Heading>
-    </>
-  );
+    return (
+        <>
+            <Container
+                h="100vh"
+                d="flex"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <MotionBox
+                    as="aside"
+                    animate={{
+                        scale: [1, 2, 2, 1, 1],
+                        rotate: [0, 0, 270, 270, 0],
+                        borderRadius: ['20%', '20%', '50%', '50%', '20%']
+                    }}
+                    transition={{
+                        duration: 2,
+                        ease: 'easeInOut',
+                        times: [0, 0.2, 0.5, 0.8, 1],
+                        repeat: Infinity,
+                        repeatType: 'loop',
+                        repeatDelay: 1
+                    }}
+                    padding="2"
+                    bgGradient="linear(to-l, #7928CA, #FF0080)"
+                    width="12"
+                    height="12"
+                    display="flex"
+                />
+            </Container>
+        </>
+    );
 };
 
-export default Home;
+export default withAuth(Home);
